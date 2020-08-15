a84c691e987f754b8b7c7262636e4967702cc188
yuvaraj2016 <juvaraj2016@gmail.com>
2020-08-14 18:52:08 -0700

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -506,7 +506,7 @@ margin:0!important;
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-address-card" style="margin-left:14px!important;"></i>
+                                <i class="fa fa-address-card" style="margin-left:17px!important;"></i>
                               Catalogue
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
