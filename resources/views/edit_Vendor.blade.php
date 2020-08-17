922b16024b41f9f6f367dde80d0559cd5495dad8
satha091 <sathasivam@hridhamtech.com>
2020-08-17 13:35:43 +0530

    edit

--- resources/views/edit_Vendor.blade.php
+++ resources/views/edit_Vendor.blade.php
@@ -87,7 +87,7 @@
                                                         </div>
                                                         <div class="col-sm-4">
                                                             <label class="col-form-label text-md-right ">Click below to edit images</label><br>
-                                                            <a href="{{ url('vendors/'.$vendors['id'].'/edit/assets') }}" class="btn btn-blue font1">Edit Image</a>
+                                                            <a href="{{ url('vendors/'.$vendors['id'].'/edit/assets') }}" class="btn btn-blue">Edit Image</a>
                                                         </div>
 
                                                         <div class="col-sm-4">
@@ -199,9 +199,9 @@
                             <div class="form-group row mb-4">
                                 <label class="col-form-label text-md-right "></label>
                                 <div class="col-sm-12 col-md-7 offset-5">
-                                    <button type="submit" class="btn btn-blue font1">Update </button>
+                                    <button type="submit" class="btn btn-blue">Update </button>
                                     <a href="{{ url('vendor_list') }}"
-                                    class=" d-inline text-center btn btn-black font1 back" ><i
+                                    class=" d-inline text-center btn btn-blue back" ><i
                                         class="icofont icofont-arrow-left" ></i>Back&nbsp;&nbsp;</a>
                                 </div>
                             </div>
