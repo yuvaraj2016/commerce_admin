3296ebbf02995fcb79c94d6e660498a56300ea0e
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-04 21:30:04 -0700

    update

--- resources/views/vendorstores_list.blade.php
+++ resources/views/vendorstores_list.blade.php
@@ -66,13 +66,7 @@
             {{ session('success') }}
         </div>
         @endif
-        @if(session('error') !== null)
-            @foreach(session('error') as $k =>$v)
-                <div class='alert alert-danger'>
-                    {{ $v[0] }}
-                </div>
-            @endforeach
-        @endif -->
+        -->
                                             
                                                 <!-- HTML5 Export Buttons table start -->
                                                 <div class="card">
